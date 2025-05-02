86eaf4c57f453c5160451d5d3520e3d955e8a108
tdphucvn <60518893+tdphucvn@users.noreply.github.com>
2025-05-02 18:26:52 +0200

    Time: 19 ms (53.38%) | Memory: 60.6 MB (20.13%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -4,38 +4,25 @@
  * @return {boolean}
  */
 function canConstruct(ransomNote, magazine) {
-  let res = true;
-  const ransomNoteMap = new Map();
-  const magazineMap = new Map();
-
-  for (let ch of ransomNote) {
-    if (ransomNoteMap.has(ch)) {
-      ransomNoteMap.set(ch, ransomNoteMap.get(ch) + 1);
-    } else {
-      ransomNoteMap.set(ch, 1);
-    }
-  }
+  const charMap = new Map();
 
   for (let ch of magazine) {
-    if (magazineMap.has(ch)) {
-      magazineMap.set(ch, magazineMap.get(ch) + 1);
+    if (charMap.has(ch)) {
+      charMap.set(ch, charMap.get(ch) + 1);
     } else {
-      magazineMap.set(ch, 1);
+      charMap.set(ch, 1);
     }
   }
 
-  ransomNoteMap.forEach((value, key) => {
-    if (!magazineMap.get(key) || magazineMap.get(key) < value) {
-      console.log(
-        `The key ${key} is violating the rule, since the total number of occurances is ${value}, however, only appears ${magazineMap.get(
-          key
-        )} in the magazine`
-      );
-      res = false;
+  for (let ch of ransomNote) {
+    if (charMap.has(ch) && charMap.get(ch) > 0) {
+      charMap.set(ch, charMap.get(ch) - 1);
+    } else {
+      return false;
     }
-  });
+  }
 
-  return res;
+  return true;
 }
 
 console.log(
