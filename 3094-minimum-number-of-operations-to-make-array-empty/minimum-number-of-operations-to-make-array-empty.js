bdd3c90a5ec72151d4496c492e33ae06d7d52574
tdphucvn <60518893+tdphucvn@users.noreply.github.com>
2025-05-05 19:30:50 +0200

    Time: 156 ms (6.25%) | Memory: 75 MB (9.38%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.js
@@ -0,0 +1,65 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minOperations(nums) {
+  const numberCounts = new Map();
+
+  for (const num of nums) {
+    if (numberCounts.has(num)) {
+      numberCounts.set(num, numberCounts.get(num) + 1);
+    } else {
+      numberCounts.set(num, 1);
+    }
+  }
+
+  let res = 0;
+  let impossible = false;
+
+  numberCounts.forEach((count, num) => {
+    console.log(
+      `The number "${num}" is represented ${count} times in the given array`
+    );
+
+    if (count < 2) {
+      impossible = -1;
+    }
+    const numOfOps = getRequiredOperations(count);
+    res += numOfOps;
+  });
+
+  console.log(impossible ? -1 : res);
+  return impossible ? -1 : res;
+}
+
+function getRequiredOperations(count) {
+  if (count % 3 === 0) {
+    return count / 3;
+  } else {
+    const isEven = count % 2 === 0;
+
+    if (isEven) {
+      const y = Math.max(
+        Math.floor(count / 3) % 2 === 0
+          ? Math.floor(count / 3)
+          : Math.floor(count / 3) - 1,
+        0
+      );
+
+      const x = (count - y * 3) / 2;
+
+      return x + y;
+    } else {
+      const y = Math.max(
+        Math.floor(count / 3) % 2 !== 0
+          ? Math.floor(count / 3)
+          : Math.floor(count / 3) - 1,
+        0
+      );
+
+      const x = (count - y * 3) / 2;
+
+      return x + y;
+    }
+  }
+}
