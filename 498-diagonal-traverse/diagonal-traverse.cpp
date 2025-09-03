5daf24996fbfeb55361a78adefdf0734591288eb
Philip Tran <60518893+tdphucvn@users.noreply.github.com>
2025-09-03 14:05:41 +0200

    Time: 3 ms (47.60%) | Memory: 22.3 MB (93.22%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -3,24 +3,22 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
        if (mat.empty() || mat[0].empty()) return {};
         int m = (int)mat.size(), n = (int)mat[0].size();
-        vector<vector<int>> diags(m + n - 1);
-
-        for (int i = 0; i < m; ++i) {
-            const auto& row = mat[i];
-            for (int j = 0; j < n; ++j) {
-                diags[i + j].push_back(row[j]);
-            }
-        }
-
         vector<int> res;
         res.reserve(m * n);
-        for (int k = 0; k < (int)diags.size(); ++k) {
-            const auto& d = diags[k];
-            if ((k & 1) == 0) {
-                // even diagonal: reverse order
-                for (int t = (int)d.size() - 1; t >= 0; --t) res.push_back(d[t]);
+
+        int r = 0, c = 0;
+        bool up = true;
+
+        for (int k = 0; k < m * n; ++k) {
+            res.push_back(mat[r][c]);
+            if (up) {
+                if (c == n - 1) { ++r; up = false; }
+                else if (r == 0) { ++c; up = false; }
+                else { --r; ++c; }
             } else {
-                for (int t = 0; t < (int)d.size(); ++t) res.push_back(d[t]);
+                if (r == m - 1) { ++c; up = true; }
+                else if (c == 0) { ++r; up = true; }
+                else { ++r; --c; }
             }
         }
         return res;
